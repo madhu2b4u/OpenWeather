8709ebc757650ebd276772fba1a2f85da76bdec6
M MADHU KALYAN <raydriiger@gmail.com>
2019-12-25 10:08:44 +0300

    TestCases

---
+++ app/src/test/java/com/demo/openweather/weather/WeatherRemoteDataSourceTest.kt
@@ -0,0 +1,112 @@
+package com.demo.openweather.weather
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.demo.openweather.BuildConfig
+import com.demo.openweather.MainCoroutineRule
+import com.demo.openweather.weather.data.model.*
+import com.demo.openweather.weather.data.remote.services.WeatherService
+import com.demo.openweather.weather.data.remote.source.WeatherRemoteDataSource
+import com.demo.openweather.weather.data.remote.source.WeatherRemoteDataSourceImpl
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import retrofit2.Response
+
+@ExperimentalCoroutinesApi
+class WeatherRemoteDataSourceTest {
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+
+    lateinit var weatherRemoteDataSource: WeatherRemoteDataSource
+
+
+    lateinit var weatherService: WeatherService
+
+    val coord = Cordinates(12.45,17.89)
+    val weather = arrayListOf<Weather>(Weather("desc","icon",788,"Drixxle"))
+    val clouds = Clouds(11)
+    val visibility = 10000
+    val main = Main(280.1,11,19,23.0,33.9,33.9)
+    val wind = Wind(1,1.7)
+    val dt =1191919
+    val sys= Sys("IT",6756,1,1,1)
+    val timezone= 3600
+    val id = 1
+    val name = "Hawali"
+    val cod= 200
+    val weatherResponse = WeatherResponse("stations",clouds,cod,coord,dt,id,main,name,sys,timezone,visibility,weather,wind)
+
+    val cityName = "Hyderabad"
+
+
+    @Before
+    fun init() {
+
+        weatherService = mock {
+            onBlocking { getCityTemperatureAsync(cityName, BuildConfig.API_KEY) } doReturn GlobalScope.async {
+                Response.success(weatherResponse)
+            }
+        }
+
+        weatherRemoteDataSource = WeatherRemoteDataSourceImpl(weatherService, mainCoroutineRule.coroutineContext)
+
+
+    }
+
+    @Test
+    fun testGetArticle() = runBlocking {
+
+        weatherService = mock {
+            onBlocking { getCityTemperatureAsync(cityName, BuildConfig.API_KEY) } doReturn GlobalScope.async {
+                Response.success(weatherResponse)
+            }
+        }
+
+        weatherRemoteDataSource = WeatherRemoteDataSourceImpl(weatherService, mainCoroutineRule.coroutineContext)
+
+
+        // Will be launched in the mainThreadSurrogate dispatcher
+        val result = weatherRemoteDataSource.getCityTemperature(cityName)
+
+
+
+        assert(result == weatherResponse)
+
+
+    }
+
+    @Test(expected = Exception::class)
+    fun testThrowArticleException() = runBlocking {
+
+        weatherService = mock {
+            onBlocking { getCityTemperatureAsync(cityName, BuildConfig.API_KEY) } doReturn GlobalScope.async {
+                Response.error<WeatherResponse>(404, null)
+            }
+        }
+
+        weatherRemoteDataSource = WeatherRemoteDataSourceImpl(weatherService, mainCoroutineRule.coroutineContext)
+
+
+
+        val result = weatherRemoteDataSource.getCityTemperature(cityName)
+
+
+
+
+    }
+
+
+}
---
+++ app/src/test/java/com/demo/openweather/weather/WeatherUseCaseTest.kt
@@ -0,0 +1,116 @@
+package com.demo.openweather.weather
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import com.demo.openweather.LiveDataTestUtil
+import com.demo.openweather.MainCoroutineRule
+import com.demo.openweather.weather.data.model.*
+import com.demo.openweather.weather.data.repository.WeatherRepository
+import com.demo.openweather.weather.domain.WeatherFragmentUseCase
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Rule
+import org.junit.Test
+import com.demo.openweather.common.Result
+import com.demo.openweather.common.Status
+import com.demo.openweather.weather.domain.WeatherFragmentUseCaseImpl
+
+@ExperimentalCoroutinesApi
+class WeatherUseCaseTest {
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+
+    private lateinit var getArticlesUseCase: WeatherFragmentUseCase
+
+    private lateinit var articleRepository: WeatherRepository
+
+
+    val coord = Cordinates(12.45,17.89)
+    val weather = arrayListOf<Weather>(Weather("desc","icon",788,"Drixxle"))
+    val clouds = Clouds(11)
+    val visibility = 10000
+    val main = Main(280.1,11,19,23.0,33.9,33.9)
+    val wind = Wind(1,1.7)
+    val dt =1191919
+    val sys= Sys("IT",6756,1,1,1)
+    val timezone= 3600
+    val id = 1
+    val name = "Hawali"
+    val cod= 200
+    val weatherResponse = WeatherResponse("stations",clouds,cod,coord,dt,id,main,name,sys,timezone,visibility,weather,wind)
+
+    val cityName = "Hyderabad"
+
+
+
+
+
+    @Test
+    fun testLoadingData()=mainCoroutineRule.runBlockingTest{
+            articleRepository = mock {
+                onBlocking { getCityTemperature(cityName) } doReturn object : LiveData<Result<WeatherResponse>>() {
+                    init {
+                        value = Result.loading()
+                    }
+                }
+            }
+            getArticlesUseCase = WeatherFragmentUseCaseImpl(articleRepository)
+
+            val result = getArticlesUseCase.getTemperature(cityName)
+
+            result.observeForever {  }
+
+            assert(LiveDataTestUtil.getValue(result).status == Status.LOADING)
+
+        }
+
+
+    @Test
+    fun testSuccessData()=mainCoroutineRule.runBlockingTest{
+        articleRepository = mock {
+            onBlocking { getCityTemperature(cityName) } doReturn object : LiveData<Result<WeatherResponse>>() {
+                init {
+                    value = Result.success(weatherResponse)
+                }
+            }
+        }
+        getArticlesUseCase = WeatherFragmentUseCaseImpl(articleRepository)
+
+        val result = getArticlesUseCase.getTemperature(cityName)
+
+        result.observeForever {  }
+
+        assert(LiveDataTestUtil.getValue(result).status == Status.SUCCESS && LiveDataTestUtil.getValue(result).data == weatherResponse)
+
+    }
+
+    @Test
+    fun testErrorData()=mainCoroutineRule.runBlockingTest{
+        articleRepository = mock {
+            onBlocking { getCityTemperature(cityName) } doReturn object : LiveData<Result<WeatherResponse>>() {
+                init {
+                    value = Result.error("error")
+                }
+            }
+        }
+        getArticlesUseCase = WeatherFragmentUseCaseImpl(articleRepository)
+
+        val result = getArticlesUseCase.getTemperature(cityName)
+
+        result.observeForever {  }
+
+        assert(LiveDataTestUtil.getValue(result).status == Status.ERROR && LiveDataTestUtil.getValue(result).message == "error")
+
+    }
+
+
+}
